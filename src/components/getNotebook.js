d77a40df709068ab8823771999622adfdcbdbda3
dhowe <dhowe@cims.nyu.edu>
2024-06-24 12:24:36 +0100

    fetch functions

--- src/components/getNotebook.js
+++ src/components/getNotebook.js
@@ -1,17 +1,27 @@
 import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+
 const parent = document.getElementById("notebook");
+
 function getNotebook(define) {
   new Runtime().module(define, name => {
-    const container = document.createElement("div");
-    if (name) {
-      container.id = name;
-      console.log("name:", name);
-      if (name.startsWith("_")) container.style.display = "none";
-    }
-    parent.append(container);
-    const inspctr = new Inspector(container);
-    // if (name == "occurrencesMap") console.log(inspctr); // DEBUG etc.
-    return inspctr; // was just: new Inspector(container); // DEBUG etc.
+    const _node = document.createElement("div");
+    const _inspector = new Inspector(_node);
+    _inspector._fulfilled = _inspector.fulfilled;
+    _inspector.fulfilled = (value) => {
+      _inspector._fulfilled(value, name);
+      if (typeof value === 'function') {
+        let pre = document.createElement("pre");
+        let code = document.createElement("code");
+        code.className = "language-js";
+        code.appendChild(document.createTextNode(value.toString()));
+        pre.appendChild(code);
+        _node.appendChild(pre);
+        //prismjs.highlightElement(code);
+      }
+    };
+    parent.appendChild(_node);
+    return _inspector;
   });
 }
+
 export { getNotebook }
